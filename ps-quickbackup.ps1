34ea36991bfada3d5959c4c86b160920209685d7
kuokkae <44156893+kuokkae@users.noreply.github.com>
2021-06-10 23:16:20 +0800

    scripts

---
+++ ps-quickbackup.ps1
@@ -0,0 +1,12 @@
+#Gets Time
+$timestamp = Get-Date -Format "dd-MMM-yyyy HH-mm-ss"
+
+#back up these files, i always keep 3 copies: backup, working and distribution
+$compress = @{
+  Path = 	"pathname\filename1",
+		"pathname\filename2",
+		"pathname\filename3",
+  CompressionLevel ="Fastest"
+  DestinationPath = 	"pathname\archive\"+$timestamp+".Zip"
+}
+Compress-Archive @compress
